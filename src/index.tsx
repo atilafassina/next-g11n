5f79f71a3d2ca34edc5756c8c1d938410115f2cb
Atila Fassina <atila@fassina.eu>
2021-10-08 19:04:21 +0200

    feat: add `ssrG11n` for better SSR support

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { getLocale } from './modules/get-locale'
 import { translator } from './modules/translator'
+import type { GetStaticPropsContext } from 'next'
 import { useRouter } from 'next/router'
 
 export function useG11n<Dictionary>(
@@ -21,3 +22,18 @@ export function useG11n<Dictionary>(
     ),
   }
 }
+
+
+export function ssrG11n<TranslationKey extends string, Locale extends string>(
+  key: TranslationKey,
+  dictionary: Record<keyof Omit<GetStaticPropsContext['locales'], 'undefined'>, string>,
+): Record<Locale, string> {
+  
+  return Object.keys(dictionary).reduce<Record<string, string>>((keySet, locale) => {
+    keySet[locale] = (dictionary[locale as keyof typeof dictionary][key])
+
+    return keySet
+  }, {})
+}
+
+export { getLocale }
